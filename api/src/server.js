d25230fa73aba7c2d434970b9b4f53499442528f
juancruzroldan95 <juancruz.roldan19@gmail.com>
2024-05-23 16:48:17 -0300

    Test change 2

--- api/src/server.js
+++ api/src/server.js
@@ -38,6 +38,7 @@ app.use(morgan('dev'));
 app.use((req, res, next) => {
 
   // res.header('Access-Control-Allow-Origin', BASE_URL);
+  
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
